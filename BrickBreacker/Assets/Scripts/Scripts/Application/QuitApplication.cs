509b3037b7524b238d5e2ea10ad3ff1c9eab7941
Riley Anderson <10803452@uvu.edu>
2024-03-18 17:18:21 -0600

    Realized Game had some useless stuff

--- BrickBreacker/Assets/Scripts/Scripts/Application/QuitApplication.cs
+++ BrickBreacker/Assets/Scripts/Scripts/Application/QuitApplication.cs
@@ -5,25 +5,11 @@ namespace Scripts
 {
     public class QuitApplication : MonoBehaviour
     {
-        public IntData scoreKey;
         public void QuitAplication()
         {
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
-#else
-		    Application.Quit ();
 #endif
-        }
-
-        public void SaveData()
-        {
-            //PlayerPrefs.SetInt(scoreKey);
-            PlayerPrefs.Save();
-        }
-
-        public void LoadData()
-        {
-        
         }
     }
 }
