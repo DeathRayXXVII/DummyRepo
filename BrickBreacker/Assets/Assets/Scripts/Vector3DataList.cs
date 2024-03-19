03427e55916064172a14c45ce9839d38e9deff7c
Riley Anderson <10803452@uvu.edu>
2024-03-18 18:44:26 -0600

    removed some dups

--- BrickBreacker/Assets/Assets/Scripts/Vector3DataList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class Vector3DataList : ScriptableObject
-{
-  public List<Vector3> vector3List;
-}
